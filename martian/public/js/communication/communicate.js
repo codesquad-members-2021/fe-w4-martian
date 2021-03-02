840dfaab9f42c553e1067707cabfc70719fd06db
adelakim <diddnjs02@likelion.org>
2021-03-03 00:45:43 +0900

    [Add] Roulette
    - 지구로 보내기 버튼 누르면 화살표 이동하는거 테스트 구현
    - 화살표 position을 위해 container 추가
    - 16진수 숫자들 모아둔 배열 만들어서 util.js에 저장
    - css 배치 추가

--- martian/public/js/communication/communicate.js
+++ martian/public/js/communication/communicate.js
@@ -6,18 +6,25 @@ const registerEvent = (type, element, ...fns) => element.addEventListener(type, 
 
 const send = (contents, receivedContent) =>
   contents.split("").forEach((letter, idx) => setTimeout(() => (receivedContent.value += letter), 500 * (idx + 1)));
-// 시간을 계속 지연시켜주기.. 바보.. 왜 이런걸로 삽질 ㅜㅜ
 
 const communicate = (senders, receivers) => {
   const { sentContentHex, sendToEarthButton } = senders;
   const { receivedContentHex, translatorButton, receivedContentText } = receivers;
   let translatedWord = ``;
+  let initDeg = 15;
 
   const convertKeydown = (e) => (isString(e) ? (translatedWord += textToHex(e)) : (translatedWord = sentContentHex.value));
   const convertKeyup = (e) => (isString(e) ? (sentContentHex.value = translatedWord) : sentContentHex.value);
   const sendToEarth = () => {
     const contents = sentContentHex.value;
     send(contents, receivedContentHex);
+    // 이 부분에서 룰렛 rotate 하는거 해야할 것 같음
+    // 5초 간격으로 문자를 받으면 -> 해당 문자에 룰렛 화살표가 2초간 머묾
+    // 가까운 방향으로 회전 -> 문자별로 시계방향이 더 빠른지, 반시계방향이 더 빠른지 계산하는 알고리즘 짜기
+    // const arrow = document.querySelector(".roulette__arrow");
+    // arrow.style.transition = `300ms`;
+    // arrow.style.transform = `translate3d(-50%, -50%, 0px) rotate(${(initDeg += 15)}deg)`;
+    // promise 사용하기
     sentContentHex.value = ``;
     translatedWord = ``;
   };
@@ -29,17 +36,6 @@ const communicate = (senders, receivers) => {
   registerEvent("keyup", sentContentHex, convertKeyup);
   registerEvent("click", sendToEarthButton, sendToEarth);
   registerEvent("click", translatorButton, translate);
-  // registerEvent("click", sendToEarthButton, () => {
-  //   const contents = sentContentHex.value;
-  //   send(contents, receivedContentHex);
-  //   sentContentHex.value = ``;
-  //   translatedWord = ``;
-  // }); // keyup, keydown 처럼 별도의 함수로 빼면 receivedContentHex가 잡히지 않는다. 방법이 없을까..? -> 잘만됨;
-
-  // registerEvent("click", translatorButton, () => {
-  //   receivedContentText.innerText += `${hexToText(receivedContentHex.value)}\n`;
-  //   receivedContentHex.value = ``;
-  // });
 };
 
 export { communicate };
---
+++ martian/public/js/util.js
@@ -0,0 +1,3 @@
+const hexadecimals = [...Array(10).keys(), "A", "B", "C", "D", "E", "F"];
+
+export { hexadecimals };

d0463a59b9ce70c986830db21b93423ff5fae573
cchoongh <cndgh49@naver.com>
2021-03-02 17:03:50 +0900

    Config dev environment using webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: {
+    app: './src/js/app.js',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  }
+};

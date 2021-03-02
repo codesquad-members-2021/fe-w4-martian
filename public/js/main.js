5edc9f5118a98f718de27b7d7d2d1bef16b5dcdf
Hongbi <dyongdi@gmail.com>
2021-03-02 21:42:43 +0900

    Add. 원형판 그리기 - svg 사용

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+const sign = document.querySelectorAll('.circular-sign');
+const signSvg = document.querySelectorAll('.sign-svg');
+
+console.log(signSvg);
+signSvg[0].children[0].attributes.fill.value = "#fff58a";

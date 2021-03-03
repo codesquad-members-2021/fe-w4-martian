693a067e4f3f292b26015b7f467a125eb6f59be1
Hongbi <dyongdi@gmail.com>
2021-03-03 21:04:57 +0900

    Feat. 텍스트를 입력하면 실시간으로 16진수로 바꿔 화면에 표시

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,18 @@
 import { strToHex, hexToStr } from './converter.js';
 import { _ } from './util.js';
 
-const $pathArr = _.$All('path');
-console.log($pathArr);
-$pathArr[0].style.fill = "#fff58a";
+const $pathArr = Array.from(_.$All('path')).slice(0, 16);
+const $marsInput = _.$('.mars-send-input');
+const $earthInput = _.$('.earth-send-input');
+const [ $hexCodeOfMessage_mars, $hexCodeOfMessage_earth ] = _.$All('.send-receive-info');
+
+const lightColor = "#fff58a";
+$pathArr[0].style.fill = lightColor;
+
+$marsInput.addEventListener('input', () => {
+  $hexCodeOfMessage_mars.textContent = strToHex($marsInput.value);
+});
+
+$earthInput.addEventListener('input', () => {
+  $hexCodeOfMessage_earth.textContent = strToHex($earthInput.value);
+})

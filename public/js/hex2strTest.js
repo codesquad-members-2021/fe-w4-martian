dbdc76a86b363ad930bda3251b7646743caa1db6
swing-park <ongsim123@gmail.com>
2021-03-03 21:46:44 +0900

    UI 구현 완료 , 기능 구현 중

--- public/js/hex2strTest.js
+++ public/js/hex2strTest.js
@@ -1,6 +1,6 @@
 const arr = ["0x48", "0x45", "0x4c", "0x4c", "0x4f"];
 
-function hex2str(hexArr) {
+function hex2strTest(hexArr) {
     const hexStr = hexArr.toString();
     let str = "";
     for (let i = 0; i < hexStr.length; i += 5) {
@@ -10,4 +10,5 @@ function hex2str(hexArr) {
     return str;
 
 }
-hex2str(arr)
+
+export { arr, hex2strTest };

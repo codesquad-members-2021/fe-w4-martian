cc56c09d6b4cd9808cdd10e83f1bc5291f464802
cchoongh <cndgh49@naver.com>
2021-03-04 01:26:18 +0900

    Complete receive logic
    * not yet implemented 'interpreting' feature and 'transmitting'

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,23 @@
 import '../scss/style.scss';
 // import variables from '../scss/_export.scss'; -> not working..
 import { _ } from './util.js';
-import { initPies, initArrowContainer, rotateArrow } from './transceiver.js';
+import * as Transceiver from './transceiver.js';
 import { initFormContainer } from './form.js';
 
 const SERVER = 'http://localhost:3000/';
 
-export const COMMUNICATION_INTERVAL = 1000;
-export const BLINK_INTERVAL = 400;
-export const PIE_CNT = 16;
+const COMMUNICATION_INTERVAL = 1000;
+const BLINK_INTERVAL = 200;
+const BLINK_CNT = 4;
+const PIE_CNT = 16;
 
 document.addEventListener('DOMContentLoaded', () => {
   fetch(SERVER + 'json/contents.json')
     .then(res => res.json())
     .then(json => {
-      const $transceiver = _.$('.txrx');
-      initPies($transceiver)(PIE_CNT);
-      initArrowContainer($transceiver)(json.arrowImg);
+      const $txrx = _.$('.txrx');
+      Transceiver.initPies($txrx)(PIE_CNT);
+      Transceiver.initArrowContainer($txrx)(json.arrowImg);
 
       initFormContainer(_.$('.receive-cont'))({
         title: '송수신정보', 
@@ -27,6 +28,13 @@ document.addEventListener('DOMContentLoaded', () => {
         title: '발신정보입력',
         btnContent: '지구로 보내기',
         btnClass: 'transmit-btn',
+      });
+
+      Transceiver.runTransceiver($txrx, {
+        serverUrl: SERVER,
+        commInterval: COMMUNICATION_INTERVAL,
+        blinkInterval: BLINK_INTERVAL,
+        blinkCnt: BLINK_CNT,
       });
 
       // rotateSimul();
--- src/js/transceiver.js
+++ src/js/transceiver.js
@@ -1,13 +1,18 @@
 import { _ } from './util.js';
-// import { MyPromise } from './my-promise.js';
+import MyPromise from './my-promise.js';
 
-const initPies = ($txrx) => {
+export const initPies = ($txrx) => {
+  const $dividerContainer = $txrx.firstElementChild;
+  const $hexContainer = $txrx.lastElementChild;
+
   return (pieCnt) => {
+    $txrx.setAttribute('data-pie-count', pieCnt);
+
     for (let i = 0; i < pieCnt; i++) {
-      $txrx.appendChild(_.genEl('DIV', {
+      $dividerContainer.appendChild(_.genEl('DIV', {
         classNames: [`divider-${i}`],
       }));
-      $txrx.appendChild(_.genEl('DIV', {
+      $hexContainer.appendChild(_.genEl('DIV', {
         classNames: [`hex-${i}`],
         template: `${i.toString(16).toUpperCase()}`,
       }));
@@ -15,7 +20,7 @@ const initPies = ($txrx) => {
   };
 }
 
-const initArrowContainer = ($txrx) => {
+export const initArrowContainer = ($txrx) => {
   return (imgData) => {
     const $container = _.genEl('DIV', {
       classNames: [`arrow-cont`],
@@ -31,37 +36,92 @@ const initArrowContainer = ($txrx) => {
   };
 }
 
-const rotateArrow = ($arrow, totalHex) => {
-  let currHex = $arrow.dataset.indicating;
-  let oldRotateClass;
-  let newRotateClass;
+export const runTransceiver = ($txrx, { serverUrl, commInterval, blinkInterval , blinkCnt}) => {
+  const $receiveForm = _.$('.form', $txrx.nextElementSibling);
+  const $arrow = _.$('.arrow', $txrx);
+  const $hexs = _.$('.hex-cont', $txrx).children;
+  const pieCnt = $txrx.dataset.pieCount;
 
-  return (hex) => {
-    const isClock = Math.abs(currHex - hex) < Math.abs(totalHex - hex) + currHex;
-    newRotateClass = `rotate-${isClock ? 'clock' : 'unclock'}-for-${hex}`;
-
-    $arrow.dataset.indicating = currHex = hex;
-    $arrow.classList.remove(oldRotateClass);
-    $arrow.classList.add(newRotateClass);
-
-    oldRotateClass = newRotateClass;
+  const rotate = () => {
+    let currHex = $arrow.dataset.indicating;
+    let oldRotateClass;
+    let newRotateClass;
+  
+    return (hex) => {
+      const isClock = Math.abs(currHex - hex) < Math.abs(pieCnt - 1 - hex) + currHex;
+      newRotateClass = `rotate-${isClock ? 'clock' : 'unclock'}-for-${hex}`;
+  
+      $arrow.dataset.indicating = currHex = hex;
+      $arrow.classList.remove(oldRotateClass);
+      $arrow.classList.add(newRotateClass);
+  
+      oldRotateClass = newRotateClass;
+    }
   }
-}
+  const rotateTo = rotate();
 
-const blink = ($hex) => {
-  // TODO
-}
+  const blink = (codes, { onFulfilled}) => {
+    const $currHex = $hexs[codes[0]];
+    let cnt = 0;
 
-const transfer = () => {
-  
-}
+    const setTimeoutBlink = () => {
+      setTimeout(() => {      
+        if ($currHex.classList.contains('blink')) {
+          $currHex.classList.remove('blink');
+          cnt++;
+        } else {
+          $currHex.classList.add('blink');
+        }
+        
+        if (cnt < blinkCnt)
+          setTimeoutBlink();
+        else
+          onFulfilled(codes.length ? codes.slice(1) : null);
+      }, blinkInterval);
+    }
 
-const receive = () => {
-  
-}
+    setTimeoutBlink();
+  }
 
-export {
-  initPies,
-  initArrowContainer,
-  rotateArrow,
+  const run = () => {
+    let intervalHandle;
+
+    new MyPromise((resolve, reject) => {
+      intervalHandle = setInterval(() => {
+        fetch(serverUrl + 'charCode')
+          .then(res => res.text())
+          .then(data => {
+            if (data) {
+              clearInterval(intervalHandle);
+              resolve([...data]);
+            }
+          });
+      }, commInterval);
+    })
+      .then(data => data.map(code => parseInt(code, 16)))
+      .then(codes => {
+        rotateTo(codes[0]);
+        return new MyPromise((resolve, reject) => {
+          $receiveForm.value += codes[0].toString(16).toUpperCase();
+          blink(codes, { onFulfilled: resolve } );
+        });
+      })
+      .then(codes => {
+        if (codes.length === 0)
+          return;
+
+        rotateTo(codes[0]);
+        return new MyPromise((resolve, reject) => {
+          $receiveForm.value += codes[0].toString(16).toUpperCase();
+          blink(codes, { onFulfilled: resolve } );
+        });
+      })
+      .catch(console.error)
+      .finally(() => {
+        $receiveForm.value += ' ';
+        run();
+      });
+  }
+
+  run();
 }

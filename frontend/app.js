f1c4ebfe307c037321243b74eb2f0140cec38c80
Jaeuk-YOO <ryou9305@naver.com>
2021-03-03 19:41:22 +0900

    [fix] hotfix 프로미스 테스트코드 제거

--- frontend/app.js
+++ frontend/app.js
@@ -11,17 +11,7 @@ const App = ({ $target }) => {
   
   const mark = MarkWatney.value; // 제너레이터
   mark.said();
-
-  const testPromise = new MyPromise((res, rej) => {
-    res("안녕하신가.");
-  });
-  console.log(testPromise)
-  testPromise.then((value) => {
-    console.log(value);
-    return value+"?";
-  }).then((v) => {
-    console.log(v);
-  })
+  
 }
 
 export default App;

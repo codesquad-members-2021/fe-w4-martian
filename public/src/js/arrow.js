04f9d29a361cae44961e67423983ae6dfa93ed1e
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-03-03 14:42:04 +0900

    Feat. 스트링 to Hex 배열 함수

--- public/src/js/arrow.js
+++ public/src/js/arrow.js
@@ -1,14 +1,22 @@
+import { _ } from './util/util.js';
+const arrow = _.$('.arrow');
 const isRightDir = (idx, targetIdx) => {
   const length = 16;
   const rightDir = Math.abs(idx - targetIdx);
   const leftDir = targetIdx > idx ? length + idx - targetIdx : length - idx + targetIdx;
-  return rightDir > leftDir;
+  return rightDir < leftDir;
 };
 
 //dir이 true이면 시계방향
-const getAngle = ({ targetIdx, dir = true }) => {
+const getAngle = (targetIdx, dir) => {
   const defaultAngle = 11.5;
   const oneAngle = 360 / 16;
   let angle = defaultAngle + targetIdx * oneAngle;
   return dir ? angle : angle - 360;
+};
+
+export const rotateArrow = (idx, targetIdx) => {
+  const dir = isRightDir(idx, targetIdx);
+  const arrowAngle = getAngle(targetIdx, dir);
+  arrow.style.transform = `rotate(${arrowAngle}deg)`;
 };

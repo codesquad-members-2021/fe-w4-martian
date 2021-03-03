f741aeb2dc56f9e4cabfdb6ed26daf97a8613df0
Seohyoung <prophecy803@gmail.com>
2021-03-03 20:08:49 +0900

    Add function which handles effect on wheel when message is submitted

--- martian/public/javascripts/index.js
+++ martian/public/javascripts/index.js
@@ -1,18 +1,39 @@
 const $charText = document.querySelector('.main--char--text');
 const $hexText = document.querySelector('.main--hex--text');
+const $sendBtn = document.querySelector('.main--char--button');
+let hexText = '';
 
 const translasteToHex = (e) => {
-  const charText = [...e.target.value];
-  const hexText = charText.reduce((acc, cur) => {
+  const arrCharText = [...e.target.value];
+  hexText = arrCharText.reduce((acc, cur) => {
     return `${acc}${cur.charCodeAt(0).toString(16)}`;
   }, '');
-  $hexText.value = hexText;
+  showTranslatedText(hexText);
 };
 
-const sendMessage = () => {};
+const showTranslatedText = (text) => {
+  $hexText.value = text;
+};
 
-const handleArrowOnWheel = () => {};
+const handleArrowOnWheel = () => {
+  const $chartText = document.querySelectorAll('.chart-text');
+  const hexTextByOne = [...hexText];
+  for (let i = 0; i < hexTextByOne.length; i++) {
+    setTimeout(() => {
+      console.log('hi');
+      // $chartText[0].classList.add('foo');
+    }, 2000);
+  }
+};
 
 const handleHexMessageBox = () => {};
 
-$charText.addEventListener('keydown', translasteToHex);
+const renderMessage = () => {
+  handleArrowOnWheel();
+  handleHexMessageBox();
+};
+
+$charText.addEventListener('keyup', translasteToHex);
+$sendBtn.addEventListener('click', renderMessage);
+
+//   const hexTextByTwo = hexText.match(/..?/g);

f7a9dfdb6a0dc50e75cfb5a80d6b1bb18a742a12
1nk_0 <>
2021-03-03 16:54:51 +0900

    ADD transmission
    * ADD convert 16 to ASCII (hex2a)
    * ADD convert ASCII to 16 (a2Hex)

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
-import {getCanvas} from '/js/canvas.js';
-getCanvas();
+// import {getCanvas} from '/js/canvas.js';
+// getCanvas();
+
+
+const receptInp = document.querySelector(".recept__inp");
+const receptBtn = document.querySelector(".recept__btn");
+
+const sendInp = document.querySelector(".send__inp");
+const sendBtn = document.querySelector(".send__btn");
 

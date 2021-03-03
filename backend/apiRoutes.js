2a3c1a55f166f4db3aa8e141c309ad47ad168fd4
Jaeuk-YOO <ryou9305@naver.com>
2021-03-03 11:06:45 +0900

    [add] backend project apiRoutes file update

---
+++ backend/apiRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+const router = express.Router();
+
+const __dirname = path.resolve();
+
+// const homeContentsData = fs.readFileSync(`${__dirname}/backend/data/homeContents.json`, 'utf8', (err, data) => {
+//   return data;
+// });
+// JSON.parse(fs.readFileSync("data.json").toString()).length;
+
+// router.get("/home-contents", (req, res) => {
+//   return res.status(200).send(homeContentsData);
+// });
+
+export default router;

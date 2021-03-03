17c107b0141d139d27bca4443c3db505cb292f8b
GleamingStar <tjdduddl10@gmail.com>
2021-03-03 18:15:00 +0900

    Docs : set fundamental HTML structure
    draw wheel by main.js with canvas

--- MyPromise.js
+++ MyPromise.js
@@ -1,24 +1,20 @@
 class MyPromise {
     constructor(callback) {
-        callback(this.resolve.bind(this), this.reject.bind(this))
+        callback(this.resolve.bind(this), this.reject.bind(this));
         this.queue = [];
     }
     resolve(input) {
-        this.execute(input)
+        this.execute(input);
     }
-    reject(input) {
-
-    }
+    // reject(input) {}
     then(cb) {
-        this.queue.push(cb)
+        this.queue.push(cb);
         return this
     }
-    catch(cb) {
-
-    }
+    // catch(cb) {}
     execute(input) {
         if (this.queue.length === 0) return
-        const result = this.queue.shift()(input)
+        const result = this.queue.shift()(input);
         result instanceof MyPromise
             ? this.chain(result)
             : this.execute(result)
@@ -30,7 +26,7 @@ class MyPromise {
     }
 }
 
-console.log("start")
+console.log("start");
 
 new MyPromise((resolve, reject) => {
     setTimeout(() => {
@@ -42,30 +38,30 @@ new MyPromise((resolve, reject) => {
     .then((v) => v + ' wonder how long chaining will increase')
     .then((v) => console.log('output : ' + v))
 
-console.log("end")
+console.log("end");
 
 const cb1 = () => new MyPromise((resolve, reject) => {
     setTimeout(() => {
-        console.log('cb1')
-        resolve('cb1')
+        console.log('cb1');
+        resolve('cb1');
     }, 5000)
 })
 const cb2 = (v) => new MyPromise((resolve, reject) => {
     setTimeout(() => {
-        console.log(v+' -> cb2')
-        resolve(v+' -> cb2')
+        console.log(v + ' -> cb2');
+        resolve(v + ' -> cb2');
     }, 2000)
 })
 const cb3 = (v) => new MyPromise((resolve, reject) => {
     setTimeout(() => {
-        console.log(v+' -> cb3')
-        resolve(v+' -> cb3')
+        console.log(v + ' -> cb3');
+        resolve(v + ' -> cb3');
     }, 2000)
 })
 const cb4 = (v) => setTimeout(() => {
-    console.log(v+' -> cb4')
+    console.log(v + ' -> cb4');
 }, 3000)
 
-cb1().then(cb2).then(cb3).then(cb4)
+cb1().then(cb2).then(cb3).then(cb4);
 
 

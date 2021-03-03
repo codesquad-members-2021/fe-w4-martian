4c3126a2a366634a4d071c4edbcb91548168b342
dudn1933 <75162982+dudn1933@users.noreply.github.com>
2021-03-04 00:46:57 +0900

    문자열 16진수 변환

--- public/javascripts/transceiver.js
+++ public/javascripts/transceiver.js
@@ -6,12 +6,33 @@ const _ = {
     contains: (node, className) => node.classList.contains(className)
 }
 
-const PROMPT = _.$(".prompt1");
+const SEND_E_M = _.$(".send_earth_mars");
+const SEND_M_E = _.$(".send_mars_earth");
+const HEX_PROMPT1 = _.$(".prompt1");
+const STRING_PROMPT1 = _.$(".prompt2");
+const HEX_PROMPT2 = _.$(".prompt3");
+const STRING_PROMPT2 = _.$(".prompt4");
 
-PROMPT.addEventListener("keyup", (e) => {
-    if(e.key === 'Enter') {
-        const array = [];
-        array.push(PROMPT.value.split(" "));
-        
+SEND_E_M.addEventListener("keyup", (e) => {
+if(e.key === 'Enter') {
+    let string = SEND_E_M.value.split("");
+    let hex = '';
+    for(const v of string) {
+        hex += `${(v.charCodeAt(0)).toString(16).toUpperCase()} `;
     }
-})
+    console.log(hex)
+    SEND_E_M.value = `${hex}`;
+}});
+
+
+
+
+
+
+// PROMPT.addEventListener("keyup", (e) => {
+//     if(e.key === 'Enter') {
+//         const array = [];
+//         array.push(PROMPT.value.split(" "));
+        
+//     }
+// })

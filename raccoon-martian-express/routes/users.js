0680e5cd8fe725a2cd778439c986e8a9772167bf
Juddroid <nobllizdc@gamil.com>
2021-03-05 00:04:43 +0900

    손으로 복구 ㅡㅡ^2

--- raccoon-martian-express/routes/users.js
+++ raccoon-martian-express/routes/users.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 module.exports = router;
-=======
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
->>>>>>> raccoon1

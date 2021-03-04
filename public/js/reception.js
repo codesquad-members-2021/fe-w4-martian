fc836bb9644fd4527e2aa51633966a9c092733aa
1nk_0 <>
2021-03-04 22:56:36 +0900

    ADD txt_hex class in index.js

--- public/js/reception.js
+++ public/js/reception.js
@@ -1,19 +1,17 @@
-import {factors} from "./common.js";
+import {factors, txt} from "./common.js";
+
 const rotateState = {
     currPoint:0,
     currDeg:22.5,
 };
+
 const signalToReception = (ele,i) =>{
     //일단 요소 다가져와 놓고 나중에 빼기 
-
 
     const hexArray = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
     new Promise ((res,rej)=>{
         setTimeout(()=>{
-            console.log("a",a);
-            a = a.toUpperCase();
-            const index = hexArray.indexOf(a);
-            console.log("index",index);
+        
             const hexCode = document.querySelectorAll(".line__txt");
 
             //hexDic 생성{수신기 값: index}

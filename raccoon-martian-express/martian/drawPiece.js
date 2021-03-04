20500f586384beff1e008169058497d8beabf796
Juddroid <nobllizdc@gamil.com>
2021-03-04 23:13:32 +0900

    rebase를 벗어나지를 못하고 있다

--- raccoon-martian-express/martian/drawPiece.js
+++ raccoon-martian-express/martian/drawPiece.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const $roulette = document.querySelector('#roulette');
 
 const hexCode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
@@ -16,22 +15,3 @@ function drawPiece() {
 }
 
 drawPiece();
-=======
-const $roulette = document.querySelector('#roulette');
-
-const hexCode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-
-function drawPiece() {
-  let pieceDiv = ``;
-  for (let i = 0; i < hexCode.length; i++) {
-    pieceDiv += `
-    <div class="piece-${i}">
-      <span class="hex-code hex-${hexCode[i]}">${hexCode[i]}</span>
-    </div>
-    `;
-  }
-  return $roulette.insertAdjacentHTML('beforeend', pieceDiv);
-}
-
-drawPiece();
->>>>>>> 8c937d0164fa300d25f3b7b973d8be512f2b97b5

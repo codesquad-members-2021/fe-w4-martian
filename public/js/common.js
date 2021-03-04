da544e14283fd7c133478293c17cf960a58e9938
1nk_0 <>
2021-03-05 01:32:45 +0900

    ADD rotation event
    * ADD arrow rotation in reception.js
    * ADD arrow rotate near by current point

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,5 @@
 const factors = {
-    arrow: document.querySelector(".arrow"),
+    arrow : document.querySelector("#arrow"),
     receptInp : document.querySelector(".recept__inp"),
     receptBtn : document.querySelector(".recept__btn"),
 
@@ -8,6 +8,9 @@ const factors = {
     sendTr : document.querySelector(".send__trans"),
 
 }
+
+
+
 
 const txt ={
     '0' : document.querySelector('.txt_0'),
@@ -20,14 +23,14 @@ const txt ={
     '7' : document.querySelector('.txt_7'),
     '8' : document.querySelector('.txt_8'),
     '9' : document.querySelector('.txt_9'),
-    'a' : document.querySelector('.txt_A'),
-    'b' : document.querySelector('.txt_B'),
-    'c' : document.querySelector('.txt_C'),
-    'd' : document.querySelector('.txt_D'),
-    'e' : document.querySelector('.txt_E'),
-    'f' : document.querySelector('.txt_F')
+    'A' : document.querySelector('.txt_A'),
+    'B' : document.querySelector('.txt_B'),
+    'C' : document.querySelector('.txt_C'),
+    'D' : document.querySelector('.txt_D'),
+    'E' : document.querySelector('.txt_E'),
+    'F' : document.querySelector('.txt_F')
 }
 
-console.log("txt는머",txt);
+
 
 export {factors, txt};
--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,12 @@
 
 import {factors, txt} from "./common.js";
 import {transmission} from "./transmission.js"
-import {signalToReception,LightOn} from "./reception.js"
+// import {signalToReception,LightOn,turn} from "./reception.js"
 const {sendInp, sendBtn, sendTr, receptInp, receptBtn} = factors;
 
-
+import {signalToReception,turn,LightOn} from "./reception.js"
 transmission(factors);
+
 // signalToReception(factors);
 
 

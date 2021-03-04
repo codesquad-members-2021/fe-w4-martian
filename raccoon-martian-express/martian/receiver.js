8324cabae17389d983f13d4ef209b390abf355e9
Juddroid <nobllizdc@gamil.com>
2021-03-04 22:03:58 +0900

    일단 해보자2

--- raccoon-martian-express/martian/receiver.js
+++ raccoon-martian-express/martian/receiver.js
@@ -1,4 +1,8 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> 2ff62f7... 이상하게 됐따... 큰일이군..
 const $earthInterpretButton = document.querySelector('#earth-interpret--button');
 const $marsInterpretButton = document.querySelector('#mars-interpret--button');
 const $earthInput = document.querySelector('#earth--input');
@@ -7,6 +11,7 @@ const $earthInfo = document.querySelector('#earth--info');
 const $marsInfo = document.querySelector('#mars--info');
 const $send2earthButton = document.querySelector('#send-to-earth--button');
 const $send2marsButton = document.querySelector('#send-to-mars--button');
+<<<<<<< HEAD
 const $arrow = document.querySelector('.arrow-circle');
 const $roulette = document.querySelector('.roulette');
 
@@ -34,11 +39,14 @@ const colorSet = {
   danger: '#dc3545',
   dark: '#212529',
 };
+=======
+>>>>>>> 2ff62f7... 이상하게 됐따... 큰일이군..
 
 $earthInterpretButton.addEventListener('click', interpretor);
 $marsInterpretButton.addEventListener('click', interpretor);
 $earthInput.addEventListener('keyup', immiInterpretor);
 $marsInput.addEventListener('keyup', immiInterpretor);
+<<<<<<< HEAD
 $earthInput.addEventListener('click', initInput);
 $marsInput.addEventListener('click', initInput);
 $send2marsButton.addEventListener('click', send2mars);
@@ -48,6 +56,11 @@ function initInput(e) {
   if (e.currentTarget.value.length > 0) e.currentTarget.value = '';
 }
 
+=======
+$send2marsButton.addEventListener('click', send2mars);
+$send2earthButton.addEventListener('click', send2earth);
+
+>>>>>>> 2ff62f7... 이상하게 됐따... 큰일이군..
 function immiInterpretor(e) {
   const isEarthInput = () => e.target.id === 'earth--input';
   const isMarsInput = () => e.target.id === 'mars--input';
@@ -78,6 +91,7 @@ function hex2str(hex) {
 
 function sendMessage() {}
 
+<<<<<<< HEAD
 function initDom(dom) {
   if (dom.value.length > 0) dom.value = '';
 }
@@ -147,6 +161,20 @@ function pieceAnamation(i) {
     $roulette.querySelector(`.piece-${i}`).style.transition = `1s ease-in-out`;
   }, 1000);
 }
+=======
+function send2mars() {
+  return moveArrow($earthInfo.value);
+}
+function moveArrow(value) {
+  setHexData(value);
+}
+
+function setHexData(value) {
+  $marsInfo.value = value;
+}
+
+function send2earth() {}
+>>>>>>> 2ff62f7... 이상하게 됐따... 큰일이군..
 
 let helloHex = str2hex('hello');
 
@@ -155,9 +183,15 @@ console.log(`test str: ${hex2str(helloHex)}`);
 
 // ===== 여기는 지구 =====
 
+<<<<<<< HEAD
 // v 1. 지구에서 메세지가 발신된다.
 // v 2. 메세지가 작성될 때, INFO에 변환 메세지가 나타난다.
 //   ( 화성에서 화살표가 잘 작동하는지 보기 위해서 )
+=======
+// 1. 지구에서 메세지가 발신된다.
+// 2. 메세지가 작성될 때, INFO에 변환 메세지가 나타난다.
+//  ( 화성에서 화살표가 잘 작동하는지 보기 위해서 )
+>>>>>>> 2ff62f7... 이상하게 됐따... 큰일이군..
 
 // ===== 여기는 화성 =====
 
@@ -167,6 +201,7 @@ console.log(`test str: ${hex2str(helloHex)}`);
 //    2-1. 없으면 다시
 // 3. 큐에 담긴 메세지를 하나씩 화살표에 전달한다.
 //    3-1. 전달된 메세지는 큐에서 삭제
+<<<<<<< HEAD
 //v 4. 화살표가 2초마다 hex-code를 가리킨다.
 //   v 4-1. 배경이 깜빡거리는 애니메이션이 발생한다.
 //   v 4-2. 글씨도 반전된다.
@@ -181,6 +216,22 @@ console.log(`test str: ${hex2str(helloHex)}`);
 // v 9. 버튼을 누르면 화살표가 2초간격으로 메세지를 가리킨다.
 // v 10. 화살표가 메세지를 가리킬때 지구의 INFO에 문자가 바로 출력된다.
 // v 11. Interpret하면 해석한다.
+=======
+// 4. 화살표가 2초마다 hex-code를 가리킨다.
+//    4-1. 배경이 깜빡거리는 애니메이션이 발생한다.
+//    4-2. 글씨도 반전된다.
+//    4-3. 화살표에도 애니메이션이 있다.
+//    4-4. 화살표는 가까운 방향으로 회전한다.
+// 5. 화살표가 hex-code를 가리킬 때, INFO에 글자가 나타난다.
+// 6. 송신이 완료되고 (if promise가 5초간 펜딩이면) Interpret 버튼을 활성화시킨다.
+// 7. 버튼을 누르면 해석이 된다.
+
+// <발신모드>
+// 8. input에 메세지가 입력되면 info에 실시간으로 해석된다.
+// 9. 버튼을 누르면 화살표가 2초간격으로 메세지를 가리킨다.
+// 10. 화살표가 메세지를 가리킬때 지구의 INFO에 문자가 바로 출력된다.
+// 11. Interpret하면 해석한다.
+>>>>>>> 2ff62f7... 이상하게 됐따... 큰일이군..
 
 class RaccoonPromise {
   constructor() {
@@ -206,6 +257,7 @@ class RaccoonPromise {
     console.log('너의 실패 이유다!');
   }
 }
+<<<<<<< HEAD
 =======
 
 const $earthInterpretButton = document.querySelector('#earth-interpret--button');
@@ -326,3 +378,5 @@ class RaccoonPromise {
   }
 }
 >>>>>>> raccoon1
+=======
+>>>>>>> 2ff62f7... 이상하게 됐따... 큰일이군..

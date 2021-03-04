54d1ce63b121ce1ab70e46c106bc1e0411692666
1nk_0 <>
2021-03-04 22:56:36 +0900

    ADD txt_hex class in index.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
 // import {getCanvas} from '/js/canvas.js';
 // getCanvas();
 
-import {factors} from "./common.js";
+import {factors, txt} from "./common.js";
 import {transmission} from "./transmission.js"
-import {rotateArrow,LightOn} from "./reception.js"
+import {signalToReception,LightOn} from "./reception.js"
 const {sendInp, sendBtn, sendTr, receptInp, receptBtn} = factors;
 
 
 transmission(factors);
-// rotateArrow(factors);
+// signalToReception(factors);
 
 

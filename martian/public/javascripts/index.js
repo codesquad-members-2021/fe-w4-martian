98548c724d36b46f4d6e823b0506a1b7ddde6942
Seohyoung <prophecy803@gmail.com>
2021-03-04 22:33:44 +0900

    Fix handle wheel function so that it calulate the direction to turn

--- martian/public/javascripts/index.js
+++ martian/public/javascripts/index.js
@@ -1,46 +1,83 @@
+/* eslint-disable arrow-body-style */
 import _ from './util.js';
 import MyPromise from './myPromise.js';
 
 const $charText = _.$('.main--char--text');
 const $hexText = _.$('.main--hex--text');
 const $sendBtn = _.$('.main--char--button');
+const $arrow = _.$('.arrow--img');
+const $text = {
+  0: _.$('.text-0'),
+  1: _.$('.text-1'),
+  2: _.$('.text-2'),
+  3: _.$('.text-3'),
+  4: _.$('.text-4'),
+  5: _.$('.text-5'),
+  6: _.$('.text-6'),
+  7: _.$('.text-7'),
+  8: _.$('.text-8'),
+  9: _.$('.text-9'),
+  A: _.$('.text-A'),
+  B: _.$('.text-B'),
+  C: _.$('.text-C'),
+  D: _.$('.text-D'),
+  E: _.$('.text-E'),
+  F: _.$('.text-F'),
+};
 let hexText = '';
+
+//const charToDec = (char) => char.charCodeAt(0)
+//const decToHex = (dec) => dec.toString(16)
 
 const translasteToHex = (e) => {
   const arrCharText = [...e.target.value];
   hexText = arrCharText.reduce((acc, cur) => {
-    return `${acc}${cur.charCodeAt(0).toString(16)}`;
+    return `${acc}${cur.charCodeAt(0).toString(16).toUpperCase()}`;
   }, '');
   showText(hexText);
 };
 
 const showText = (text) => ($hexText.value = text);
 
-const handleArrowOnWheel = () => {
+const handleWheel = () => {
   const $chartText = _.$All('.chart-text');
-  let hexTyping = '';
   const hexTextByOne = [...hexText];
+  let hexTyping = '';
 
-  let asyncFn = (i) => {
+  let asyncFn = (val) => {
     return new Promise((resolve, reject) => {
-      if (i === 0) {
+      if (val === 0) {
         $hexText.value = 0;
       }
 
-      hexTyping += hexTextByOne[i];
+      hexTyping += val;
       showText(hexTyping);
-      $chartText[hexTextByOne[i]].classList.add('blink');
+      $text[val].classList.add('blink');
+
+      const hexToDec = parseInt(val, 16);
+      const initAngle = -78.5;
+      const [positiveAngleToTurn, negativeAngleToTurn] = [
+        hexToDec * 22.5,
+        -360 + hexToDec * 22.5,
+      ];
+      const angleToTurn =
+        positiveAngleToTurn < 180
+          ? initAngle + positiveAngleToTurn
+          : initAngle + negativeAngleToTurn;
+
+      const transformDeg = `rotate(${angleToTurn}deg)`;
+      $arrow.style.setProperty('transform', transformDeg);
 
       setTimeout(() => {
-        $chartText[hexTextByOne[i]].classList.remove('blink');
+        $text[val].classList.remove('blink');
         resolve(true);
       }, 2000);
     });
   };
 
-  hexTextByOne.reduce((acc, cur, index) => {
+  hexTextByOne.reduce((acc, cur) => {
     return acc.then(() => {
-      return asyncFn(index);
+      return asyncFn(cur);
     });
   }, Promise.resolve());
 };
@@ -48,7 +85,7 @@ const handleArrowOnWheel = () => {
 const handleHexMessageBox = () => {};
 
 const renderMessage = () => {
-  handleArrowOnWheel();
+  handleWheel();
 };
 
 _.addEvent($charText, 'keyup', translasteToHex);
--- martian/public/javascripts/util.js
+++ martian/public/javascripts/util.js
@@ -2,6 +2,8 @@ const _ = {
   $: (selector, target = document) => target.querySelector(selector),
   $All: (strSelector, target = document) =>
     target.querySelectorAll(strSelector),
+  $name: (className, target = document) =>
+    target.getElementsByClassName(className),
   addEvent: (target, eventType, callback, options = false) =>
     target.addEventListener(eventType, callback, options),
 };

82ada03b9b2eb8161f13cf332670061a252a1593
hayoung123 <67357426+hayoung123@users.noreply.github.com>
2021-03-04 14:43:00 +0900

    Feat. MyPromise Class

--- promiseTest/myPromise.js
+++ promiseTest/myPromise.js
@@ -2,6 +2,7 @@ const { log } = console;
 const arr = [1, 2, 3, 4, 5];
 const getDelay = (v, time) => new Promise((resolve) => setTimeout(() => resolve(v), time));
 
+<<<<<<< HEAD
 const cbFn = async (acc, v) => {
   const res = await getDelay(v, 1000);
   log('acc+res:', acc + res);
@@ -18,6 +19,51 @@ const reduceAsync = async (cbFn, acc, iter) => {
   }
   return acc;
 };
+=======
+class MyPromise {
+  constructor(fn) {
+    this.cbList = [];
+    this.errCbList = [];
+    this.status;
+    fn(this.resolve.bind(this), this.reject.bind(this));
+  }
+  then(cb) {
+    this.cbList.push(cb);
+    return this;
+  }
+  catch(cb) {
+    this.errCbList.push(cb);
+  }
+  resolve(value) {
+    if (this.status !== 'rejected') {
+      this.status = 'fulfilled';
+      pipe(...this.cbList)(value);
+    }
+    return this;
+  }
+  reject() {
+    if (this.status !== 'fulfilled') {
+      this.status = 'rejected';
+      if (this.errCbList.length) pipe(...this.errCbList)();
+      else console.error('rejected');
+    }
+    return this;
+  }
+}
+
+new MyPromise((res, rej) => {
+  setTimeout(() => {
+    res(1);
+    rej();
+  }, 1000);
+})
+  .then((v) => v + 1)
+  .then((v) => v + 2)
+  .then(log)
+  .catch(() => {
+    log('error');
+  });
+>>>>>>> 12aa611... Feat. MyPromise Class
 
 const print = async () => {
   const sum = await reduceAsync(cbFn, arr);
--- public/src/js/myPromise.js
+++ public/src/js/myPromise.js
@@ -36,21 +36,5 @@ class MyPromise {
     return this;
   }
 }
-// class MyPromise {
-//   constructor(initValue, delay) {
-//     this.initValue = initValue;
-//     this.cbList = [];
-//     this.initValue.forEach((v, idx) => {
-//       setTimeout(() => {
-//         const fns = pipe(...this.cbList);
-//         fns(v, idx);
-//       }, delay * idx);
-//     });
-//   }
-//   then(cb) {
-//     this.cbList.push(cb);
-//     return this;
-//   }
-// }
 
-// export default MyPromise;
+export default MyPromise;

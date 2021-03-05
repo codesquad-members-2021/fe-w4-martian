9ff64554180a85f3d5bd1928c3e68db52653ec86
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-03-05 14:01:21 +0900

    Upload : dom 선택용 유틸 객체 업로드

---
+++ martian/public/javascripts/dom.js
@@ -0,0 +1,20 @@
+const dom = {
+    hexInfo: {
+        input : document.querySelector(".hex_info_input"),
+        button : document.querySelector(".hex_info_submit")
+    },
+    
+    strInfo: {
+        input : document.querySelector(".str_info_input"),
+        button : document.querySelector(".str_info_submit")
+    },
+    
+    modeInfo: {
+        str : document.querySelector(".current_mode"),
+        button : document.querySelector(".toggle_mode")
+    },
+    
+    arrow: document.querySelector(".material-icons")
+}
+
+export {dom};

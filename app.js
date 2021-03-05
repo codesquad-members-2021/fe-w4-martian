e4788c49d14820dfe36704b61fc118c695e6bc28
leehangeul <75162982+dudn1933@users.noreply.github.com>
2021-03-05 10:54:54 +0900

    [Step2] promise는 공부중 & addEventListener로 구현중 (#56)
    * 전부다 재설치...
    
    * html 완성 js 시작
    
    * 문자열 16진수 변환
    
    * promise는 계속 공부중, addEventListener로 구현중

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -10,7 +9,6 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,9 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 app.use(cors());
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

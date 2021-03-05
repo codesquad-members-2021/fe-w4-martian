ac5db29ae1e1109827b450bf5e025084ada6795e
adelakim <diddnjs02@likelion.org>
2021-03-05 11:33:26 +0900

    Merge Conflicts

--- martian/public/js/communication/communicate.js
+++ martian/public/js/communication/communicate.js
@@ -1,12 +1,16 @@
 import { textToHex, hexToText } from "./convert.js";
+<<<<<<< HEAD
 import { rotate } from "./rotate.js";
 import { times } from "../util.js";
 import MyPromise from "../Promise.js";
+=======
+>>>>>>> e63811c ([Create] Roulette)
 
 const isString = ({ keyCode }) => (keyCode >= 65 && keyCode <= 90) || keyCode === 32;
 
 const registerEvent = (type, element, ...fns) => element.addEventListener(type, (e) => fns.forEach((fn) => fn(e)));
 
+<<<<<<< HEAD
 const response = (content, receivers) => {
   const { receivedContentHex, translatorButton } = receivers;
   content.split("").forEach((letter, i) =>
@@ -33,6 +37,11 @@ const throttle = (func, limit) => {
     }
   };
 };
+=======
+const send = (contents, receivedContent) =>
+  contents.split("").forEach((letter, idx) => setTimeout(() => (receivedContent.value += letter), 500 * (idx + 1)));
+// 시간을 계속 지연시켜주기.. 바보.. 왜 이런걸로 삽질 ㅜㅜ
+>>>>>>> e63811c ([Create] Roulette)
 
 const communicate = (senders, receivers) => {
   const { sentContentHex, sendToEarthButton } = senders;
@@ -40,18 +49,26 @@ const communicate = (senders, receivers) => {
   let translatedWord = ``;
 
   const convertKeydown = (e) => (isString(e) ? (translatedWord += textToHex(e)) : (translatedWord = sentContentHex.value));
+<<<<<<< HEAD
 
   const convertKeyup = (e) => (sentContentHex.value = isString(e) ? translatedWord : sentContentHex.value);
 
   const sendToEarth = () => {
     const content = sentContentHex.value;
     response(content, { receivedContentHex, translatorButton });
+=======
+  const convertKeyup = (e) => (isString(e) ? (sentContentHex.value = translatedWord) : sentContentHex.value);
+  const sendToEarth = () => {
+    const contents = sentContentHex.value;
+    send(contents, receivedContentHex);
+>>>>>>> e63811c ([Create] Roulette)
     sentContentHex.value = ``;
     translatedWord = ``;
   };
   const translate = () => {
     receivedContentText.innerText += `${hexToText(receivedContentHex.value)}\n`;
     receivedContentHex.value = ``;
+<<<<<<< HEAD
     translatorButton.disabled = true;
   };
 
@@ -59,6 +76,24 @@ const communicate = (senders, receivers) => {
   registerEvent("keyup", sentContentHex, throttle(convertKeyup, 100));
   registerEvent("click", sendToEarthButton, sendToEarth);
   registerEvent("click", translatorButton, translate);
+=======
+  };
+  registerEvent("keydown", sentContentHex, convertKeydown);
+  registerEvent("keyup", sentContentHex, convertKeyup);
+  registerEvent("click", sendToEarthButton, sendToEarth);
+  registerEvent("click", translatorButton, translate);
+  // registerEvent("click", sendToEarthButton, () => {
+  //   const contents = sentContentHex.value;
+  //   send(contents, receivedContentHex);
+  //   sentContentHex.value = ``;
+  //   translatedWord = ``;
+  // }); // keyup, keydown 처럼 별도의 함수로 빼면 receivedContentHex가 잡히지 않는다. 방법이 없을까..? -> 잘만됨;
+
+  // registerEvent("click", translatorButton, () => {
+  //   receivedContentText.innerText += `${hexToText(receivedContentHex.value)}\n`;
+  //   receivedContentHex.value = ``;
+  // });
+>>>>>>> e63811c ([Create] Roulette)
 };
 
 export { communicate };
--- martian/public/js/main.js
+++ martian/public/js/main.js
@@ -1,11 +1,34 @@
 import { communicate } from "./communication/communicate.js";
+<<<<<<< HEAD
 import { hexadecimals, selectors } from "./util.js";
 import { getRoulette } from "./roulette.js";
 
 const { sendToEarthButton, sentContentHex, receivedContentHex, translatorButton, receivedContentText } = selectors;
+=======
+// import Chart from "chart.js";
+import { drawRoulette } from "./chart.js";
+
+// const receivedContent = document.querySelector(".receivedContent");
+const translatorButton = document.querySelector(".translate__button");
+const sentContentHex = document.querySelector(".sentContent__hex");
+// const sentContentText = document.querySelector(".sentContent__text");
+// text도 화면에 보여주기??
+const sendToEarthButton = document.querySelector(".sendContent__button");
+const receivedContentHex = document.querySelector(".receivedContent__hex");
+const receivedContentText = document.querySelector(".receivedContent__text");
+
+>>>>>>> e63811c ([Create] Roulette)
 const senders = { sentContentHex, sendToEarthButton };
 const receivers = { receivedContentHex, translatorButton, receivedContentText };
 communicate(senders, receivers);
+// myChart;
 
+const width = 500;
+const height = 500;
+
+<<<<<<< HEAD
 const myRoulette = document.querySelector("#myRoulette");
 myRoulette.innerHTML = getRoulette(Math.floor(hexadecimals.length / 2));
+=======
+drawRoulette(width, height);
+>>>>>>> e63811c ([Create] Roulette)

da3008ea244ba460920bf3f917974702c6d0eabf
adelakim <diddnjs02@likelion.org>
2021-03-06 12:25:45 +0900

    [Create] js code of Martian-project
    - express 설치
    - 현재는 sender 폴더에서 주고 받고를 다하는 중.. 리팩토링 예정
    - sender, receiver에 해당하는 것들을 한 페이지 내에 적게 되어서... 고민해볼 예정
    - myPromise
        - 개략적인 부분만 구현
    - 현재 동작 사항
        - 문자를 치면 자동적으로 16진수로 변환
        - 지구로 보내기 버튼을 누르면 왼쪽 인풋창에 500ms마다 한 문자씩 수신(5초가 너무 길어서..)
        - 해석하기 버튼을 누르면 문자열로 해석

--- martian/routes/users.js
+++ martian/routes/users.js
@@ -1,9 +1,18 @@
+<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
+=======
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+>>>>>>> d296fcf ([Create] js code of Martian-project)
 });
 
 module.exports = router;

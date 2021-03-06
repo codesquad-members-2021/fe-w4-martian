68720aa9f98983c39222b5c63c979e35f970729a
adelakim <diddnjs02@likelion.org>
2021-03-06 12:23:22 +0900

    [Fix] Accept feedback
    - node.js : var -> const로 바꾸기
    - rotate() : 함수를 잘게 쪼개고, rotate()안에서 사용.. rotate를 함수들의 합성으로 만들어낼 수 있는 함수를 한번 도전해보고자 함
    - roulette: getLineText()를 없애고 하나로 합침

--- martian/routes/index.js
+++ martian/routes/index.js
@@ -1,26 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3385c81 ([Add] Promise)
 const express = require("express");
 const router = express.Router();
-=======
-var express = require("express");
-var router = express.Router();
->>>>>>> 32c4567 ([Add] Promise)
-<<<<<<< HEAD
-=======
-const express = require("express");
-const router = express.Router();
->>>>>>> e99ca98 ([Fix] Accept feedback)
-=======
-const express = require("express");
-const router = express.Router();
->>>>>>> 521d52e6f4034e072f3bd813a755a5a7b5318756
-=======
->>>>>>> 3385c81 ([Add] Promise)
 
 /* GET home page. */
 router.get("/", function (req, res, next) {

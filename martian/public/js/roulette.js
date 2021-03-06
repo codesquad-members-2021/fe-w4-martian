1f712eab6d28038e09203716cbf71d9f970ce35b
adelakim <diddnjs02@likelion.org>
2021-03-06 16:24:28 +0900

    [Add] Promise
    - Mypromise 구현
    - 룰렛: canvas -> html, css 기반으로 변경

--- martian/public/js/roulette.js
+++ martian/public/js/roulette.js
@@ -1,10 +1,15 @@
+// const getLineText = (num) => `<span class="line__text" data-id='${num}'>${num}</span>`;
+// const myRoulette = document.querySelector("#myRoulette");
+
+// const length = Math.floor(hexadecimals.length / 2);
+
 const drawRoulette = (length) => {
   let itemHtml = ``;
   for (let i = 0; i < length; i++) {
     itemHtml += `<div class="line line${i + 1}">
           <span class="line__text" data-id='${i}'>${i}</span>
           <span class="line__text" data-id='${(i + 8).toString(16).toUpperCase()}'>${(i + 8).toString(16).toUpperCase()}</span>
-        </div>`;
+          </div>`;
   }
   return itemHtml;
 };

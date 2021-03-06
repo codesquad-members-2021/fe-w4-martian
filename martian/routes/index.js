98373b2842918fad48e205c149012fa33e279d41
adelakim <diddnjs02@likelion.org>
2021-03-06 12:19:52 +0900

    [Add] Promise
    - Mypromise 구현
    - 룰렛: canvas -> html, css 기반으로 변경

--- martian/routes/index.js
+++ martian/routes/index.js
@@ -1,12 +1,16 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 3385c81 ([Add] Promise)
 const express = require("express");
 const router = express.Router();
 =======
 var express = require("express");
 var router = express.Router();
 >>>>>>> 32c4567 ([Add] Promise)
+<<<<<<< HEAD
 =======
 const express = require("express");
 const router = express.Router();
@@ -15,6 +19,8 @@ const router = express.Router();
 const express = require("express");
 const router = express.Router();
 >>>>>>> 521d52e6f4034e072f3bd813a755a5a7b5318756
+=======
+>>>>>>> 3385c81 ([Add] Promise)
 
 /* GET home page. */
 router.get("/", function (req, res, next) {

66aea4dd2f6d2e7d9e0629276f2bf911aa852c74
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-03-06 21:20:28 +0900

    Add : 데이터 변환 유틸 함수 추가
    * 문자열을 16진수로 변환하는 함수
    * 16진수를 10진수 로 변환하는 함수

---
+++ martian/public/javascripts/convert.js
@@ -0,0 +1,56 @@
+const getHexFromMsg = (msg) => {
+
+    const engArray = Array.from(msg);
+    const decArray = engArray.map((el) => el.charCodeAt(0));
+    const hexArray = decArray.map((el) => el.toString(16))
+    const hexStr = hexArray.join("");
+
+    return hexStr;
+}
+
+
+const getIdxFromChar = (char) => {
+    let idx;
+
+    switch (char) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            idx = Number(char);
+            break;
+
+        case "a":
+            idx = 10;
+            break;
+        case "b":
+            idx = 11;
+            break;
+        case "c":
+            idx = 12;
+            break;
+        case "d":
+            idx = 13;
+            break;
+        case "e":
+            idx = 14;
+            break;
+        case "f":
+            idx = 15;
+            break;
+
+        default:
+            console.error(`입력된 문자열 ${char} 는 16진수로 변환할 수 없습니다.`);
+            break;
+    }
+
+    return idx;
+}
+
+export {getHexFromMsg, getIdxFromChar};

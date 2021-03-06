7daa872b4d27f240dc91ae82e9551a002b28aa08
adelakim <diddnjs02@likelion.org>
2021-03-06 16:26:16 +0900

    [Add] translatorButton's ability
    - translatorButton이 disabled 였다가 메시지를 받으면 abled 하게 바꿈
    - 근데 문자를 다 받은 후에 abled로 하고싶은데.... 어케 해야 할까

---
+++ martian/public/js/communication/rotate.js
@@ -0,0 +1,87 @@
+import { hexadecimals, selectors, rotateState, times } from "../util.js";
+import MyPromise from "../Promise.js";
+
+const turn = (diff, currDeg, element, isClockWise) => {
+  element.style.transition = `${times.transition}ms`;
+  if (isClockWise) {
+    element.style.transform = `translate3d(-50%, -50%, 0) rotate(${currDeg + diff * 22.5}deg)`;
+    currDeg += diff * 22.5;
+  }
+  if (!isClockWise) {
+    element.style.transform = `translate3d(-50%, -50%, 0) rotate(${currDeg - diff * 22.5}deg)`;
+    currDeg -= diff * 22.5;
+  }
+  return currDeg;
+};
+
+const lightOn = (target, className) => target[1].classList.add(className);
+
+const lightOut = (target) => target[1].classList.remove("light");
+
+const findTextTarget = (elements, capital) => {
+  // console.log(elements);
+  return Object.entries(elements).find((item) => item[1].dataset.id === capital);
+};
+
+const getHTMLElements = (className) => document.querySelectorAll(`.${className}`);
+
+const getDiff = ({ state, key }, endPoint) => {
+  const diff = endPoint - state[key];
+  const absDiff = Math.abs(diff);
+  return { diff, absDiff };
+};
+
+const turnAsDirection = ({ state, key }, element, { diff, absDiff }) => {
+  // console.log(state, key);
+  // console.log(element);
+  // console.log(diff, absDiff);
+  let currDeg = state[key];
+  if (absDiff > 7) currDeg = diff <= 0 ? turn(16 - absDiff, currDeg, element, true) : turn(16 - absDiff, currDeg, element, false);
+  if (absDiff <= 7) currDeg = diff <= 0 ? turn(absDiff, currDeg, element, false) : turn(absDiff, currDeg, element, true);
+  return currDeg;
+};
+
+const getEndPoint = (array, target) => array.findIndex((item) => item.toString() === target);
+
+const capital = (letter) => letter.toUpperCase();
+
+const adela = (f, ...fns) => (...args) =>
+  args.length
+    ? f(
+        ...fns.reduce((acc, fn, i) => {
+          acc.push(fn(args[i]));
+          return acc;
+        }, [])
+      )
+    : f(...fns);
+
+const rotate = (letter, i, isLast) =>
+  new MyPromise((resolve, reject) =>
+    setTimeout(() => {
+<<<<<<< HEAD
+      if (rotateState.pastTarget) lightOut(rotateState.pastTarget);
+      const endPoint = getEndPoint(hexadecimals, capital(letter));
+      const target = adela(findTextTarget, getHTMLElements, capital)("line__text", letter);
+      const diff = adela(getDiff, { state: rotateState, key: "currPoint" }, endPoint);
+      const direction = adela(turnAsDirection, { state: rotateState, key: "currDeg" }, selectors.arrow, diff());
+      !isLast ? lightOn(target, "light") : lightOn(target, "last");
+      rotateState.pastTarget = target;
+      rotateState.currDeg = direction();
+      rotateState.currPoint = endPoint;
+      resolve(capital(letter));
+=======
+      console.log(`letter: ${letter.toUpperCase()}`);
+      const index = hexadecimals.findIndex((item) => item == letter.toUpperCase());
+      console.log(`index: ${index}`);
+      const diff = index - rotateState.currPoint;
+      const absDiff = Math.abs(diff);
+      if (absDiff > 7)
+        rotateState.currDeg = diff <= 0 ? turn(16 - absDiff, rotateState.currDeg, true) : turn(16 - absDiff, rotateState.currDeg, false);
+      if (absDiff <= 7) rotateState.currDeg = diff <= 0 ? turn(absDiff, rotateState.currDeg, false) : turn(absDiff, rotateState.currDeg, true);
+      rotateState.currPoint = index;
+      resolve(letter);
+>>>>>>> cbb48ed ([Add] translatorButton's ability)
+    }, times.send * (i + 1))
+  );
+
+export { rotate, lightOut };

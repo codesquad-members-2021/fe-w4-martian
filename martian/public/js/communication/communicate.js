427de9ba4cef6430132397ffdb9374c1a4fc3c38
adelakim <diddnjs02@likelion.org>
2021-03-06 16:29:04 +0900

    [Add] Roulette
    - 지구로 보내기 버튼 누르면 화살표 이동하는거 테스트 구현
    - 화살표 position을 위해 container 추가
    - 16진수 숫자들 모아둔 배열 만들어서 util.js에 저장
    - css 배치 추가

--- martian/public/js/communication/communicate.js
+++ martian/public/js/communication/communicate.js
@@ -38,6 +38,7 @@ const communicate = (senders, receivers) => {
   const { sentContentHex, sendToEarthButton } = senders;
   const { receivedContentHex, translatorButton, receivedContentText } = receivers;
   let translatedWord = ``;
+  let initDeg = 15;
 
   const convertKeydown = (e) => (isString(e) ? (translatedWord += textToHex(e)) : (translatedWord = sentContentHex.value));
 

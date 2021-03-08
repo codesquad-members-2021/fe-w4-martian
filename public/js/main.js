f89e3689897086623be690d6fe5b0b1015c41a1e
Hongbi <dyongdi@gmail.com>
2021-03-08 23:18:50 +0900

    Feat. '지구로 보내기' 버튼 클릭 시 데이터 저장
    입력칸에 메시지가 있을 때만 데이터를 저장시켰다. 데이터는 16진수로 변환된 string이며 공백을 제외하고 한 글자씩 쪼개어 배열에 담는다.

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
 import { strToHex, hexToStr } from './converter.js';
-import { $pathArr, $marsInput, $earthInput, $hexCodeOfMessage_mars, $hexCodeOfMessage_earth } from './ref.js';
+import { $contentsBox, $pathArr, $marsSendReceiveInfo, $marsInput, $marsSendBtn, $earthInput, $earthSendBtn, $hexCodeOfMessage_mars, $hexCodeOfMessage_earth } from './ref.js';
+const { log } = console;
 
-const lightColor = "#fff58a";
-$pathArr[0].style.fill = lightColor;
+// const lightColor = "#fff58a";
+// $pathArr[0].style.fill = lightColor;
 
 $marsInput.addEventListener('input', () => {
   $hexCodeOfMessage_mars.textContent = strToHex($marsInput.value);
@@ -10,4 +11,18 @@ $marsInput.addEventListener('input', () => {
 
 $earthInput.addEventListener('input', () => {
   $hexCodeOfMessage_earth.textContent = strToHex($earthInput.value);
+})
+
+$contentsBox.addEventListener('click', ({ target }) => {
+  if(target.closest('.mars-send-btn')) {
+    let sentHexMessage = $marsSendReceiveInfo.innerText;
+    if(sentHexMessage) {
+      const hexMessageArr = sentHexMessage
+                              .split('')
+                              .filter(v => v !== ' ');
+      $marsInput.value = '';
+      $marsSendReceiveInfo.innerText = '';
+      $marsSendBtn.disabled = 'true';
+    }
+  }
 })
--- public/js/ref.js
+++ public/js/ref.js
@@ -1,15 +1,23 @@
 import { _ } from './util.js';
 
+const $contentsBox = _.$('.contents-box');
 const $pathArr = Array.from(_.$All('path')).slice(0, 16);
+const $marsSendReceiveInfo = _.$('.mars-send-receive-info');
 const $marsInput = _.$('.mars-send-input');
+const $marsSendBtn = _.$('.mars-send-btn');
 const $earthInput = _.$('.earth-send-input');
+const $earthSendBtn = _.$('.earth-send-btn');
 const [ $hexCodeOfMessage_mars, $hexCodeOfMessage_earth ] = _.$All('.send-receive-info');
 const $arrow = _.$('.arrow');
 
 export {
+  $contentsBox,
   $pathArr,
+  $marsSendReceiveInfo,
   $marsInput,
+  $marsSendBtn,
   $earthInput,
+  $earthSendBtn,
   $hexCodeOfMessage_mars,
   $hexCodeOfMessage_earth,
   $arrow

3550ec1422f8c655b8b7d17a35409966c31b5c94
Hongbi <dyongdi@gmail.com>
2021-03-09 20:40:59 +0900

    Modify. 파라미터 수정, 함수 이름 변경, 주석 살짝

--- public/js/rotateArrow.js
+++ public/js/rotateArrow.js
@@ -5,7 +5,7 @@ let prevDec = 0;
 const splitStr = str => str.split('');
 const hexCharToDec = hexChar => parseInt(hexChar, 16);
 
-const getFastWay = (prevDec, currDec) => {
+const getFastWay = (prevDec, currDec) => { // hexCharToDec의 리턴값 필요
   const diff = currDec - prevDec;
   const diffAbs = Math.abs(diff);
   if (diff === 0) return 'samePosition';
@@ -19,20 +19,16 @@ const getFastWay = (prevDec, currDec) => {
   }
 }
 
-const getDegree = decimal => { // 파라미터 어떻게 해야 할지 모르겠음
-  const convertedDecNum = hexCharToDec(hexChar); // hexCharToDec의 리턴값 필요
+const getAngle = (convertedDecNum, fastWay) => { // hexCharToDec, getFastWay의 리턴값 필요
   const baseAngle = 11.25;
   const pieceAngle = 360 / 16;
   const currAngle = baseAngle + pieceAngle * convertedDecNum;
-  const fastWay = getFastWay(prevDec, currDec); // getFastWay의 리턴값 필요
   if(fastWay === 'samePosition') return currAngle;
   return (fastWay === 'clockWise' ? currAngle : 360 - currAngle);
 }
 
-const rotateArrow = (hexChar) => {
-  // getDegree의 리턴값 필요
-  const degree = getDegree();
-  $arrow.style.transform = `rotate(${degree}deg)`;
+const rotateArrow = (angle) => { // getAngle의 리턴값 필요
+  $arrow.style.transform = `rotate(${angle}deg)`;
 }
 
 const blinkLight = () => {}

5dec0c7f106c2ce16b29a8956d6deeb89c83347c
Hongbi <dyongdi@gmail.com>
2021-03-10 15:13:09 +0900

    Modify. 함수, 변수 이름 수정, 일부 커링 함수로 변경
    pipe 함수를 쓰려니 파라미터의 개수가 맞지 않는 문제가 있어서 파라미터가 하나 더 필요한 함수들은 커링함수로 표현했다.

--- public/js/rotateArrow.js
+++ public/js/rotateArrow.js
@@ -1,11 +1,9 @@
 import { _, pipe } from './util.js';
 import { $arrow } from './ref.js';
 
-let prevDec = 0;
-const splitStr = str => str.split('');
 const hexCharToDec = hexChar => parseInt(hexChar, 16);
 
-const getFastWay = (prevDec, currDec) => { // hexCharToDec의 리턴값 필요
+const getFastWay = prevDec => currDec => { // hexCharToDec의 리턴값 필요
   const diff = currDec - prevDec;
   const diffAbs = Math.abs(diff);
   if (diff === 0) return 'samePosition';
@@ -19,10 +17,10 @@ const getFastWay = (prevDec, currDec) => { // hexCharToDec의 리턴값 필요
   }
 }
 
-const getAngle = (convertedDecNum, fastWay) => { // hexCharToDec, getFastWay의 리턴값 필요
+const getAngle = currDec => fastWay => { // hexCharToDec, getFastWay의 리턴값 필요
   const baseAngle = 11.25;
   const pieceAngle = 360 / 16;
-  const currAngle = baseAngle + pieceAngle * convertedDecNum;
+  const currAngle = baseAngle + pieceAngle * currDec;
   if(fastWay === 'samePosition') return currAngle;
   return (fastWay === 'clockWise' ? currAngle : 360 - currAngle);
 }
@@ -36,4 +34,4 @@ const displayHexOnBoard = () => {}
 const disableBtn = (buttonName) => {}
 const activateBtn = (buttonName) => {}
 
-export { rotateArrow, getFastWay }
+export { renderArrow }
